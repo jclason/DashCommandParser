77afb3c741a260480485c7af2157cf6346809eff
Jared <Jared@JAREDS-PC>
2017-02-01 23:34:07 -0500

    Add project files.

---
+++ DashCommandParser/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DashCommandParser
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
